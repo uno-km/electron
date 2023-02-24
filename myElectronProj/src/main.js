9b99b5c595171ef7e9a1d818706976958101e62c
unokim <zhfldk014745@naver.com>
2023-02-24 21:46:13 +0900

    :wrench: add menu

--- myElectronProj/src/main.js
+++ myElectronProj/src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron') 
+const { app, BrowserWindow, Menu, shell } = require('electron');
 // include the Node.js 'path' module at the top of your file 
 const path = require('path') 
 // modify your existing createWindow() function 
@@ -14,7 +14,59 @@ function createWindow () {
 } 
 app.whenReady().then(() => { 
   createWindow() 
-}) 
+})
+/**
+ * @values 메뉴선언부
+*/
+/*start*/
+const template = 
+[
+  { 
+    label: "File"
+    ,submenu: 
+    [ 
+      { 
+        label   : "Open"
+        , click : (ev)=>{ 
+          console.log('Clicked Menu Open!!!');
+        }
+      }
+      ,{type : "separator"}
+      ,{ 
+        label   : "New" 
+      }
+      ,{ 
+        label: 'Dark Mode'
+        , type: 'checkbox'
+        , checked: true 
+      }
+    ] 
+  }
+  ,  { 
+    label: "Dev"
+    ,submenu: 
+    [ 
+      { 
+        role   : "toggleDevTools" 
+      }
+    ] 
+  }  ,  { 
+    label: "Help"
+    ,submenu: 
+    [ 
+      { 
+        label   : "Api" 
+        , click : (ev)=>{ 
+          shell.openExternal("https://www.electronjs.org/docs/latest/api/app");
+        }
+      }
+    ] 
+  }
+]; 
+const menu = Menu.buildFromTemplate(template); 
+/*end*/
+Menu.setApplicationMenu(menu);
+
 app.on('window-all-closed', function () { 
   if (process.platform !== 'darwin') app.quit() 
 })
