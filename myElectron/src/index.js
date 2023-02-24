d540820d660b55ec6dd6139d0ee8d6458fd7398b
unokim <zhfldk014745@naver.com>
2023-02-25 00:29:53 +0900

    :tada: create new proj

---
+++ myElectron/src/index.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, Menu, shell } = require('electron');
+//import setMenu from "script/menu/setMenu.js";
+const path = require('path') 
+function createWindow () { 
+  const win = new BrowserWindow({ 
+    width: 800, 
+    height: 600, 
+    webPreferences: { 
+      preload: path.join(__dirname, 'preload.js') 
+    } 
+  }) 
+  win.loadFile('src/index.html')
+}
+/**
+ * @values 메뉴선언부
+*/
+{
+/*start*/
+const template = 
+[
+  { 
+    label: "File"
+    ,submenu: 
+    [ 
+      { 
+        label   : "Open"
+        , click : (ev)=>{ 
+          console.log('Clicked Menu Open!!!');
+        }
+      }
+      ,{type : "separator"}
+      ,{ 
+        label   : "New" 
+      }
+      ,{ 
+        label: 'Dark Mode'
+        , type: 'checkbox'
+        , checked: true 
+      }
+    ] 
+  }
+  ,  { 
+    label: "Dev"
+    ,submenu: 
+    [ 
+      { 
+        role   : "toggleDevTools" 
+      }
+    ] 
+  }  ,  { 
+    label: "Help"
+    ,submenu: 
+    [ 
+      { 
+        label   : "Api" 
+        , click : (ev)=>{ 
+          shell.openExternal("https://www.electronjs.org/docs/latest/api/app");
+        }
+      }
+    ] 
+  }
+]; 
+const menu = Menu.buildFromTemplate(template); 
+/*end*/
+}
+app.whenReady().then(() => { 
+  createWindow() 
+})
+app.on('window-all-closed', function () { 
+  if (process.platform !== 'darwin') app.quit() 
+})
